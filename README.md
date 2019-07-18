a3db1cc2155a769dfd7237a127c540f780ec96f0
ImsirovicAjdin <ajdika@live.com>
2019-07-18 14:20:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ryauf8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ryauf8)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div>start with initial minimal template</div>
+  `
+})
+export class AppComponent  {
+
+}
